b984a7e2a0a78b02df1c3e459e7b5a4e74b5ab2a
Daniel Saleh <dasa0006@stud.kea.dk>
2025-05-08 08:19:12 +0200

    feat: implement write for database io

--- src/Database.java
+++ src/Database.java
@@ -32,14 +32,56 @@ public class Database {
         return records;
 
     }
+
+    public void write(ArrayList<Object> records, String fileName) throws IOException {
+        String path = System.getProperty("user.dir");
+        String data_dir = path+"\\data\\"+fileName;
+        File yourFile = new File(data_dir);
+        try {
+            yourFile.createNewFile(); // if file already exists will do nothing
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        };
+
+        BufferedWriter writer = new BufferedWriter(new FileWriter(data_dir, true));
+        for (Object record : records) {
+            writer.append("\n").append(record.toString());
+        }
+
+        writer.close();
+    }
 }
 
 
 /*
-EXAMPLE USAGE:
+READ EXAMPLE USAGE:
 
     Database db = new Database();
     String data_dir = "Database.csv";
     ArrayList<Object> out = db.read(data_dir);
+
+
+WRITE EXAMPLE USAGE:
+
+    ArrayList<Object> records = new ArrayList<>();
+
+    ArrayList<Object> record = new ArrayList<>();
+    record.add("Peter");
+    record.add(56);
+    record.add("male");
+    record.add("DAD");
+
+    records.add(record);
+
+    ArrayList<Object> record2 = new ArrayList<>();
+    record2.add("Lois");
+    record2.add(36);
+    record2.add("female");
+    record2.add("MOM");
+
+    records.add(record2);
+
+    System.out.println(records);
+    db.write(records, "Database.csv");
 
 */
