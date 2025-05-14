e87aff593332a44060b7f58502a00beba175bc47
monataherzadeh <monataherzadeh05@gmail.com>
2025-05-14 11:48:59 +0200

    RegistrationOfMember

--- .idea/src/Person.java
+++ .idea/src/Person.java
@@ -1,27 +1,34 @@
+import java.time.LocalDate;
+import java.time.Period;
+
 public class Person {
     private static int næsteID = 1;
     private int id;
     private String firstName;
     private String lastName;
-    private int age;
+    private LocalDate birthDate;
     private String phoneNo;
     private String address;
 
-    public Person(String firstName, String lastName, int age, String phoneNo, String address) {
+    public Person(String firstName, String lastName, LocalDate birthDate, String phoneNo, String address) {
         this.id = næsteID++;
         this.firstName = firstName;
         this.lastName = lastName;
-        this.age = age;
+        this.birthDate = birthDate;
         this.phoneNo = phoneNo;
         this.address = address;
     }
 
     @Override
     public String toString() {
-        return "ID: " + id + ", First Name: " + firstName + ", Last Name: " + lastName + ", Age: " + age + ", Phone Number: " + phoneNo + ", Address: " + address;
+        return "ID: " + id + ", First Name: " + firstName + ", Last Name: " + lastName + ", Age: " + getAge() + ", Phone Number: " + phoneNo + ", Address: " + address;
     }
 
     public int getId() {
         return id;
+    }
+
+    public int getAge() {
+        return Period.between(birthDate, LocalDate.now()).getYears();
     }
 }
