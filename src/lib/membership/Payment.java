67ab0fb802657f0a706606a294a3dcd134905c80
404BrainNotFound1000 <emilgurres@gmail.com>
2025-05-14 10:54:35 +0200

    Implementation to show payments added to payments

--- src/lib/membership/Payment.java
+++ src/lib/membership/Payment.java
@@ -2,10 +2,16 @@ package lib.membership;
 
 import lib.persons.Member;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.time.LocalDate;
 import java.time.Period;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
-public class Payment {
+public class Payment implements IFileHandler {
     private LocalDate subscriptionDate;
     private LocalDate lastPayment;
     private double paymentAmount;
@@ -62,3 +68,65 @@ public class Payment {
         this.lastPayment = lastPayment;
     }
 }
+
+//Indlæs payment data fra csv fil der indeholder alle members.
+
+    //NEED:
+    //Sort payments into two arrays, with sort on age.
+
+    //Array der holder all payments.
+    private List<Integer> allPayments = new ArrayList<>();
+
+    //Metode der indlæser all payment attributter fra én csv fil der indeholder alle Medlemmer objekter.
+    @Override
+    public void load(String filePath) {
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            String line;
+
+            while ((!line = br.nextLine) = null) {
+                String[] fields = line.split(",");
+
+                if (fields.length >= 4);
+                try {
+                    int amount = Integer.parseInt(fields[3].trim());
+                    allPaymments.add(amount);
+                }
+                catch (NumberFormatException e) {
+                    System.err.println("Ikke validt tal format i linjen: " + line);
+                }
+                else {
+                    System.err.println("Invalid line(too few fields)): " + line);
+
+                }
+            }
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public List<Integer> getAllPayments() {
+        return allPayments;
+    }
+
+    //metode til at printe betalinger enkeltvis: (Metoden indeholder load() metode der loader filen.).
+    public void printRespectivePayments() {
+        System.out.print("Her er alle de enkelte betalinger: ");
+        for (int temp : getAllPayments()) {
+            System.out.println(temp);
+        }
+    }
+
+    //Metode til at returnere summen af alle betalinger: (Metoden indeholder load() metode der loader filen.).
+    public void printSumOfPayments() {
+        int sum = 0;
+        System.out.println("Her er summen af de enkelte betalinger: ");
+        for (int temp : getAllPayments()) {
+            sum += temp;
+        }
+
+            }
+        }
+
+
+}
+
